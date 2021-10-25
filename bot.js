e1f16f907021762a407c5e2cb36f0737d12ee0b4
Glitch (twitch-chatbot) <none>
2021-10-25 16:12:01 +0000

    💼🏵 Checkpoint ./bot.js:37430677/795

--- bot.js
+++ bot.js
@@ -27,9 +27,23 @@ function onMessageHandler (target, context, msg, self) {
 
   // Remove whitespace from chat message
   const commandName = msg.trim();
+  //!echo say this!
+  if(commandName === "!helloworld") {
+    client.say(target, "Hello world!");
+  } else if(commandName.startsWith("!echo")) {
+    const sayText = commandName.replace("!echo ", "");
+    client.say(target, sayText);
+  }
 }
 
 // Called every time the bot connects to Twitch chat
 function onConnectedHandler (addr, port) {
   console.log(`* Connected to ${addr}:${port}`);
+}
+
+function isMod(user, channel) {
+  let isMod = user.mod || user["user-type"] === "mod";
+  let isBroadcaster = channel.slice(1) === user.username;
+  let isSquizzle = user.username === "squizzleflip";
+  return isMod || isBroadcaster || isSquizzle;
 }
