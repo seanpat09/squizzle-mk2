39c7ab9e0d32458c356c1bd01d3522ca811cc9f9
Glitch (twitch-chatbot) <none>
2021-10-29 16:38:22 +0000

    🦀🐋 Checkpoint ./tests/channel.test.js:37430677/530 ./channel.js:37430677/228

--- channel.js
+++ channel.js
@@ -1,10 +1,12 @@
 "use strict";
 
-module.export = class Channel {
+module.exports = class Channel {
   constructor(channelName, client) {
+    this.name = channelName;
+    this.client = client;
   }
   
   handleMessage(userstate, msg) {
-  
+    this.client.say(this.name, "squizzle bot online.")
   }
 }
--- tests/channel.test.js
+++ tests/channel.test.js
@@ -1,7 +1,9 @@
 const Channel = require('.././channel.js');
 
 const MOCK_CHANNEL_NAME = "myChannel";
-const MOCK_CLIENT = {};
+const MOCK_CLIENT = {
+  say: jest.fn()
+};
 
 describe("Channel", () => {
     afterEach(() => {
@@ -12,13 +14,18 @@ describe("Channel", () => {
       test("registration should instantiate with a client", () => {
         const aChannel = new Channel(MOCK_CHANNEL_NAME, MOCK_CLIENT);
         
-        expect(aChannel.chan
+        expect(aChannel.name).toBe(MOCK_CHANNEL_NAME);
+        expect(aChannel.client).tBe(MOCK_CLIENT);
       });
     })
 
     describe("channel commands", () => {
       test("should be able to ping bot in channel", () => {
-        expect(false).toBe(true);
+        const aChannel = new Channel(MOCK_CHANNEL_NAME, MOCK_CLIENT);
+        
+        aChannel.handleMessage({}, "!ping");
+        
+        expect(MOCK_CLIENT.say).toBeCalledWith(MOCK_CHANNEL_NAME, "squizzle bot online.");
       })
     })
 })
