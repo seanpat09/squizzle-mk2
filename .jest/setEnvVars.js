436cdc6378f440ac6ab58c1e384c2f7f8d9c43fe
Glitch (twitch-chatbot) <none>
2021-10-31 17:58:42 +0000

    🐔🎭 Checkpoint

--- .jest/setEnvVars.js
+++ .jest/setEnvVars.js
@@ -2,3 +2,5 @@
 process.env.BOT_USERNAME= "fakebotname"
 process.env.OAUTH_TOKEN= "fakeoauthtoken"
 process.env.CHANNEL_NAME= "fakechannelname"
+process.env.DB_USER = "fakeuser"
+process.env.DB_PASS = "fakepass"
